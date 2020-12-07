e2ee6ac8f03f144370cbc85866f18819b4de1c01
Wenjun Sun <wenjun.sun@gmail.com>
2020-12-08 00:13:35 +0800

    Added code to calculate entropy for angle unit of "deg".

--- entropy.py
+++ entropy.py
@@ -11,8 +11,10 @@ def print_help():
     print('e.g., 0.12527823  0.012408118')
     print('where, the first column is the orientation in radian and the second is the unnormalized probability.')
 
-def calculate_entropy(orientations, frequencies):
+def calculate_entropy(orientations, frequencies, unit):
     bin_size = orientations[1]-orientations[0]
+    if unit == 'deg':
+        bin_size = bin_size*math.pi/180
     print('Bin size (radian): ',bin_size)
     orientation_entropy = entropy(frequencies)+math.log(bin_size)
     #orientation_entropy = entropy(frequencies-min(frequencies))+math.log(bin_size)
@@ -22,6 +24,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("delimiter", help="delimiter separating data in a row")
     parser.add_argument("skiprows", type=int, help="number of rows to skip")
+    parser.add_argument("unit", help="Unit of angle (rad|deg)")
     parser.add_argument("filename", help="name of the data file")
     args = parser.parse_args()
 
@@ -30,9 +33,10 @@ if __name__ == '__main__':
         sys.exit(-1)
     delimiter = args.delimiter
     skiprows = args.skiprows
+    unit = args.unit
     filename = args.filename
     try:
         orientations, frequencies= np.loadtxt(filename, usecols = (0,1), unpack=True, delimiter=delimiter, skiprows=skiprows)
-        calculate_entropy(orientations, frequencies)
+        calculate_entropy(orientations, frequencies, unit)
     except Exception as e:
         print(f'Error: {e}.')
